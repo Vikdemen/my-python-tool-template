a460db6cf45329c5ca757574dec5db09f636fd1e
Demenev Viktor <viktor.demen@gmail.com>
2021-07-31 15:04:05 +0300

    Initial commit

---
+++ {{cookiecutter.package_slug}}/README.md
@@ -0,0 +1,3 @@
+##{{cookiecutter.package_name}}
+###What is it?
+{{cookiecutter.description}}
---
+++ {{cookiecutter.package_slug}}/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='{{cookiecutter.package_name}}',
+    version='0.1.0',
+    package_dir={'': 'src'},
+    packages=['{{cookiecutter.package_name}}'],
+    url='{{cookiecutter.gh_url}}',
+    license='MIT',
+    author='{{cookiecutter.full_name}}',
+    author_email='{{cookiecutter.email}}',
+    description='{{cookicutter.description}}',
+    python_requires='>= 3.9'
+)
